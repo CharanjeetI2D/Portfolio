2852e72777b24cf462973ad599c2e5a76bb86eb4
CharanjeetI2D <charanjeet8149@gmail.com>
2024-07-17 18:22:12 +0530

    Create columns, image and descriptino

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/Myphoto.jpeg")
+
+with col2:
+    st.title("Charanjeet Singh")
+    content = """Hi, I am Charanjeet! I am a Python Programmer, Data Analyst, Traveller.
+    I have worked in different spaces like Data analysis, Market Research, Recruitment, BI developer
+    and now Python Programmer."""
+    st.info(content)
+
