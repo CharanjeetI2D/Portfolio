2ab262f16c28fa6cedb898ad04795298ea309513
CharanjeetI2D <charanjeet8149@gmail.com>
2024-07-18 16:34:40 +0530

    Added descriptions, images and links

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 st.set_page_config(layout='wide')
 
@@ -21,14 +21,19 @@ st.write(content2)
 
 col3, col4 = st.columns(2)
 
-df = pandas.read_csv("data.csv", sep=';')
+df = pd.read_csv("data.csv", sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
-    for index,row in df[10:].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
-
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://www.google.com))")
