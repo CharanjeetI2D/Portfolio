fb93661c76053f21edde165b8e4e39c622d14f04
CharanjeetI2D <charanjeet8149@gmail.com>
2024-07-22 22:17:03 +0530

    Day23: send emails using forms

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 8501
+
+    user_name = "charanjeet.rec@gmail.com"
+    sender = "charanjeet.rec@gmail.com"
+    password = "utljjxbdvgjmkbaw"
+
+    receiver = "charanjeet.rec@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP("smtp.gmail.com") as server:
+        server.starttls() and server.login(user=user_name, password=password)
+        server.sendmail(from_addr=sender,
+                        to_addrs=receiver,
+                        msg=message)
